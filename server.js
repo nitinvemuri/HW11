c487722513273b04ae91adf2f8e24a37583bdf5e
nitinvemuri <80983324+nitinvemuri@users.noreply.github.com>
2021-06-06 01:42:01 -0700

    routes

--- server.js
+++ server.js
@@ -5,10 +5,36 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static('db'));
 app.use(express.static('public'));
 
-app.get()
+const notes = require('./Develop/db/db.json')
+
+
+//ROUTES _____________________________________________________________________________________________________________
+app.get('/api/notes', (req,res) => {
+    res.json(notes.slice(1));
+});
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './Develop/public'))
+});
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+});
+
+function createNotes(body,notesArray) {
+    const newNotes = body;
+    if (!Array.isArray(notesArray))
+    notesArray = [];
+}
 
 const PORT = process.env.PORT || 3002;
 
